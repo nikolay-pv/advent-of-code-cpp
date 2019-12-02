269c22faf82e4640a00b3b5e54a5446df5060456
nikolayp <nikolay.a.petrov@gmail.com>
2019-12-02 21:36:44 +0100

    Second part of the second task is finished.

--- 2019/main.cpp
+++ 2019/main.cpp
@@ -35,7 +35,7 @@ void performCalculation(std::vector<long>::iterator begginingOfInstruction,
     }
 }
 
-void performSimulation(std::vector<long> computerMemory, long noun, long verb)
+long performSimulation(std::vector<long> computerMemory, long noun, long verb)
 {
     computerMemory[1] = noun;
     computerMemory[2] = verb;
@@ -51,10 +51,11 @@ void performSimulation(std::vector<long> computerMemory, long noun, long verb)
         performCalculation(instrctionPointer, computerMemory);
         instrctionPointer += numOfValuesInInstruction;
     }
-    cout << "The result of the programm:" << endl;
-    for_each(computerMemory.cbegin(), computerMemory.cend(), [](const auto& v){ cout << v << ", ";});
-    cout << endl << "The first position:" << endl;
-    cout << computerMemory[0] << endl;
+    //cout << "The result of the programm:" << endl;
+    //for_each(computerMemory.cbegin(), computerMemory.cend(), [](const auto& v){ cout << v << ", ";});
+    //cout << endl << "The first position:" << endl;
+    //cout << computerMemory[0] << endl;
+    return computerMemory[0];
 }
 
 int main()
@@ -65,8 +66,25 @@ int main()
     std::vector<long> computerMemory(N);
     for(int input = 0; input != N; ++input)
         cin >> computerMemory[input];
-    long noun = 12, verb = 2;
-    performSimulation(computerMemory, noun, verb);
+    const long target = 19690720;
+    long noun = 13, verb = 2;
+    long result = performSimulation(computerMemory, noun, verb);
+    // first increase noun
+    while(result < target)
+        result = performSimulation(computerMemory, ++noun, verb);
+    if (result != target)
+    {
+        // decrease noun as we are too high and increase verb as we will be too low
+        result = performSimulation(computerMemory, --noun, ++verb);
+        // then increase verb
+        while(result < target)
+            result = performSimulation(computerMemory, noun, ++verb);
+    }
+    cout << "Noun is " << noun << endl;
+    cout << "Verb is " << verb << endl;
+    cout << "Result is " << result << endl;
+    cout << "Target is " << target << endl;
+    cout << "Answer is " << 100 * noun + verb << endl;
     return 0;
 }
 
