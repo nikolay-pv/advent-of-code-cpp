47e5b129aad5946908d998d77b7247e843028d9d
nikolayp <nikolay.a.petrov@gmail.com>
2019-12-04 00:56:07 +0100

    Fist part of task 3

--- 2019/main.cpp
+++ 2019/main.cpp
@@ -1,90 +1,101 @@
 #include <iostream>
+#include <set>
 #include <vector>
+#include <algorithm>
+#include <sstream>
 using namespace std;
 
-enum OpcodeInstruction : long
+enum Direction : char
 {
-    sum = 1,
-    multiply = 2,
-    halt = 99
+    Right = 'R',
+    Up = 'U',
+    Left = 'L',
+    Down = 'D'
 };
 
-enum ParamAddress : int
+using pll = pair<long, long>;
+pll operator+(pll lhs, const pll& rhs)
 {
-    first = 1,
-    second = 2,
-    third = 3
-};
-
-void performCalculation(std::vector<long>::iterator begginingOfInstruction,
-                        std::vector<long>& computerMemory)
-{
-    const long leftParam = *(begginingOfInstruction + ParamAddress::first);
-    const long rightParam = *(begginingOfInstruction + ParamAddress::second);
-    const long destParam = *(begginingOfInstruction + ParamAddress::third);
-    switch(*begginingOfInstruction) {
-    case OpcodeInstruction::sum:
-        computerMemory[destParam] = computerMemory[leftParam] + computerMemory[rightParam];
-        break;
-    case OpcodeInstruction::multiply:
-        computerMemory[destParam] = computerMemory[leftParam] * computerMemory[rightParam];
-        break;
-    default :
-        std::cerr << "Ups" << std::endl;
-        break;
-    }
+    lhs.first += rhs.first;
+    lhs.second += rhs.second;
+    return lhs;
 }
 
-long performSimulation(std::vector<long> computerMemory, long noun, long verb)
+pll processCommand(set<pll>& result,
+        pll start,
+        char direction, int count)
 {
-    computerMemory[1] = noun;
-    computerMemory[2] = verb;
-    auto instrctionPointer = computerMemory.begin();
-    const int numOfValuesInInstruction = 4;
-    while(*instrctionPointer != OpcodeInstruction::halt)
-    {
-        if (*instrctionPointer != OpcodeInstruction::sum && *instrctionPointer != OpcodeInstruction::multiply)
-        {
-            cout << "Houston, we have a problem!" << endl;
+    // x, y
+    pll increment{};
+    switch(direction) {
+        case Direction::Right:
+            increment = {1,0};
             break;
-        }
-        performCalculation(instrctionPointer, computerMemory);
-        instrctionPointer += numOfValuesInInstruction;
+        case Direction::Up:
+            increment = {0,1};
+            break;
+        case Direction::Left:
+            increment = {-1,0};
+            break;
+        case Direction::Down:
+            increment = {0,-1};
+            break;
     }
-    //cout << "The result of the programm:" << endl;
-    //for_each(computerMemory.cbegin(), computerMemory.cend(), [](const auto& v){ cout << v << ", ";});
-    //cout << endl << "The first position:" << endl;
-    //cout << computerMemory[0] << endl;
-    return computerMemory[0];
+    while(count > 0)
+    {
+        start = start + increment;
+        result.insert(start);
+        --count;
+    }
+    return start;
+}
+
+set<pll> processInput(std::string const& input)
+{
+    set<pll> result{};
+    stringstream ss{input};
+    char separator=',';
+    pll last{0, 0};
+    do{
+        char direction;
+        ss >> direction;
+        int count;
+        ss >> count;
+        ss >> separator;
+        last = processCommand(result, last, direction, count);
+        cerr << direction << count << separator;
+    } while(ss.good());
+    cerr << endl << "Last point: " << last.first << ", " << last.second << endl;
+    return result;
 }
 
 int main()
 {
     int N;
     cin >> N;
-    long fuel = 0;
-    std::vector<long> computerMemory(N);
-    for(int input = 0; input != N; ++input)
-        cin >> computerMemory[input];
-    const long target = 19690720;
-    long noun = 13, verb = 2;
-    long result = performSimulation(computerMemory, noun, verb);
-    // first increase noun
-    while(result < target)
-        result = performSimulation(computerMemory, ++noun, verb);
-    if (result != target)
-    {
-        // decrease noun as we are too high and increase verb as we will be too low
-        result = performSimulation(computerMemory, --noun, ++verb);
-        // then increase verb
-        while(result < target)
-            result = performSimulation(computerMemory, noun, ++verb);
-    }
-    cout << "Noun is " << noun << endl;
-    cout << "Verb is " << verb << endl;
-    cout << "Result is " << result << endl;
-    cout << "Target is " << target << endl;
-    cout << "Answer is " << 100 * noun + verb << endl;
+
+    std::string input{};
+    cin >> input;
+    set<pll> path1{processInput(input)};
+    cin >> input;
+    set<pll> path2{processInput(input)};
+
+    vector<pll> intersections{};
+    set_intersection(path1.begin(), path1.end(),
+                     path2.begin(), path2.end(),
+                     back_inserter(intersections));
+
+    vector<long> distances{};
+    transform(intersections.cbegin(), intersections.cend(),
+              back_inserter(distances),
+              [](const pll& point) -> long
+              { return abs(point.first) + abs(point.second); } );
+
+    std::sort(distances.begin(), distances.end());
+    cout << "All distances are: " << endl;
+    for_each(distances.cbegin(), distances.cend(),
+             [](const auto& el){ cout << el << ", "; });
+    cout << endl << "Answer is " << distances[0] << endl;
     return 0;
 }
 
