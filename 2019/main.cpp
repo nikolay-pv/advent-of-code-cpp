c55520fe983254806f33136f7030dfbd31e56d15
nikolayp <nikolay.a.petrov@gmail.com>
2019-12-04 08:34:41 +0100

    Forth task part one

--- 2019/main.cpp
+++ 2019/main.cpp
@@ -5,106 +5,61 @@
 #include <sstream>
 using namespace std;
 
-enum Direction : char
-{
-    Right = 'R',
-    Up = 'U',
-    Left = 'L',
-    Down = 'D'
-};
 
-using pll = pair<long, long>;
-pll operator+(pll lhs, const pll& rhs)
+bool sizeRequirements(std::string const& psswd)
 {
-    lhs.first += rhs.first;
-    lhs.second += rhs.second;
-    return lhs;
+    return psswd.size() == 6;
 }
 
-pll processCommand(vector<pll>& result,
-        pll start,
-        char direction, int count)
+bool sameDigitsRequirements(std::string const& psswd)
 {
-    // x, y
-    pll increment{};
-    switch(direction) {
-        case Direction::Right:
-            increment = {1,0};
-            break;
-        case Direction::Up:
-            increment = {0,1};
-            break;
-        case Direction::Left:
-            increment = {-1,0};
-            break;
-        case Direction::Down:
-            increment = {0,-1};
-            break;
-    }
-    while(count > 0)
-    {
-        start = start + increment;
-        result.push_back(start);
-        --count;
-    }
-    return start;
+    for(int i = 1; i != psswd.size(); ++i)
+        if (psswd[i] == psswd[i - 1])
+            return true;
+    return false;
 }
 
-vector<pll> processInput(std::string const& input)
+bool increasingDigitsRequirements(std::string const& psswd)
 {
-    vector<pll> result{};
-    stringstream ss{input};
-    char separator=',';
-    pll last{0, 0};
-    do{
-        char direction;
-        ss >> direction;
-        int count;
-        ss >> count;
-        ss >> separator;
-        last = processCommand(result, last, direction, count);
-        cerr << direction << count << separator;
-    } while(ss.good());
-    cerr << endl << "Last point: " << last.first << ", " << last.second << endl;
-    return result;
+    for(int i = 1; i != psswd.size(); ++i)
+        if (psswd[i - 1] > psswd[i])
+            return false;
+    return true;
+}
+
+bool passwordValidation(std::string const& psswd)
+{
+    // 6 digits;
+    return //sizeRequirements(psswd)
+        /*&&*/ sameDigitsRequirements(psswd)
+        && increasingDigitsRequirements(psswd);
 }
 
 int main()
 {
     int N;
     cin >> N;
+    int M;
+    cin >> M;
 
-    std::string input{};
-    cin >> input;
-    vector<pll> path1{processInput(input)};
-    cin >> input;
-    vector<pll> path2{processInput(input)};
+    const int max_pass = 999999;
+    if (N > max_pass)
+        N = max_pass;
+    if (M > max_pass)
+        M = max_pass;
 
-    vector<pll> intersections{};
+    vector<string> sutiablePsswds;
+    for(int i = N; i != M; ++i)
     {
-        set<pll> spath1{path1.begin(), path1.end()};
-        set<pll> spath2{path2.begin(), path2.end()};
-        set_intersection(spath1.begin(), spath1.end(),
-                         spath2.begin(), spath2.end(),
-                         back_inserter(intersections));
+        std::string input{to_string(i)};
+        if (passwordValidation(input))
+            sutiablePsswds.push_back(std::move(input));
     }
 
-    vector<long> steps{};
-    transform(intersections.cbegin(), intersections.cend(),
-              back_inserter(steps),
-              [&](const pll& point) -> long
-              {
-              return distance(path1.cbegin(), find(path1.cbegin(), path1.cend(), point))
-                   + distance(path2.cbegin(), find(path2.cbegin(), path2.cend(), point))
-                   // 0 based count
-                   + 2;
-               });
-
-    std::sort(steps.begin(), steps.end());
-    cout << "All steps are: " << endl;
-    for_each(steps.cbegin(), steps.cend(),
+    cout << "All paswords are: " << endl;
+    for_each(sutiablePsswds.cbegin(), sutiablePsswds.cend(),
              [](const auto& el){ cout << el << ", "; });
-    cout << endl << "Answer is " << steps[0] << endl;
+    cout << endl << "Answer is " << sutiablePsswds.size() << endl;
     return 0;
 }
 
