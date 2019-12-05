d14c5da27a2b458453ff763903c8918f0f3d998f
nikolayp <nikolay.a.petrov@gmail.com>
2019-12-05 22:03:16 +0100

    Rework the computer to be more flexible

--- 2019/main.cpp
+++ 2019/main.cpp
@@ -1,67 +1,177 @@
 #include <iostream>
-#include <set>
 #include <vector>
-#include <algorithm>
-#include <sstream>
+#include <map>
+#include <memory>
 using namespace std;
 
 
-bool sameDigitsRequirements(std::string const& psswd)
+enum OpcodeInstruction : long
 {
+    sum = 1,
+    multiply = 2,
+    halt = 99
+};
 
-    for(int i = 1; i != psswd.size(); ++i)
-    {
-        if (psswd[i] != psswd[i - 1])
-            continue;
-        if (i - 2 >= 0 && psswd[i] == psswd[i - 2])
-            continue;
-        if (i + 1 < psswd.size() && psswd[i] == psswd[i + 1])
-            continue;
-        return true;
+optional<OpcodeInstruction> castToOpcode(long value)
+{
+    switch(value) {
+    case OpcodeInstruction::sum:
+    case OpcodeInstruction::multiply:
+    case OpcodeInstruction::halt:
+        break;
+    default :
+        std::cerr << "Ups" << std::endl;
+        return nullopt;
     }
-    return false;
+    return static_cast<OpcodeInstruction>(value);
 }
 
-bool increasingDigitsRequirements(std::string const& psswd)
+class CmdExecutor
 {
-    for(int i = 1; i != psswd.size(); ++i)
-        if (psswd[i - 1] > psswd[i])
-            return false;
-    return true;
-}
+public:
+    virtual long paramsLength() = 0;
+    virtual void execute(vector<long>::iterator begginingOfInstruction,
+                         vector<long>& computerMemory) = 0;
+    virtual ~CmdExecutor() = default;
+};
+using CmdExecutorPtr = unique_ptr<CmdExecutor>;
 
-bool passwordValidation(std::string const& psswd)
+////////////////////////////////////////////////////////////////////////////////
+// CmdExecutors
+struct IntCodeComputer
 {
-    return sameDigitsRequirements(psswd)
-        && increasingDigitsRequirements(psswd);
+    static bool halting;
+    static void halt() { halting = true; };
+
+    map<OpcodeInstruction, CmdExecutorPtr> instructionSet{};
+    vector<long> memory{};
+    std::vector<long>::iterator instructionPos{};
+
+    void setMemory() { instructionPos = memory.begin(); };
+
+    long getMemoryHead() const { return memory[0]; };
+    long runningLoop(long input);
+    optional<OpcodeInstruction> getCurrentOpcode() const;
+
+    void printMemory() const;
+};
+
+bool IntCodeComputer::halting = false;
+
+void IntCodeComputer::printMemory() const
+{
+    cout << "The internal memory of the computer:" << endl;
+    for_each(memory.cbegin(), memory.cend(), [](const auto& el){ cout << el << ", ";});
+    cout << endl;
 }
 
+long IntCodeComputer::runningLoop(long input)
+{
+    long offset = 0;
+    while(!halting)
+    {
+        instructionPos += offset;
+        auto instrCode = getCurrentOpcode();
+        if (instrCode == nullopt)
+            break;
+        auto executor = instructionSet.find(instrCode.value());
+        if (executor == instructionSet.end())
+            break;
+        executor->second->execute(instructionPos, memory);
+        offset = executor->second->paramsLength();
+    }
+    if (halting)
+        return 0;
+    cout << "Houston, we have a problem!" << endl;
+    IntCodeComputer::halt();
+    return -1;
+}
+
+optional<OpcodeInstruction> IntCodeComputer::getCurrentOpcode() const
+{
+    const long code = *instructionPos;
+    return castToOpcode(code);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// CmdExecutors
+class Sum : public CmdExecutor
+{
+    enum ParamAddress : int
+    {
+        first = 1,
+        second = 2,
+        third = 3
+    };
+public:
+    long paramsLength() override { return 4; }
+    void execute(vector<long>::iterator begginingOfInstruction,
+                         vector<long>& computerMemory) override
+    {
+        const long leftParam = *(begginingOfInstruction + ParamAddress::first);
+        const long rightParam = *(begginingOfInstruction + ParamAddress::second);
+        const long destParam = *(begginingOfInstruction + ParamAddress::third);
+        computerMemory[destParam] = computerMemory[leftParam] + computerMemory[rightParam];
+    }
+};
+
+class Multiply : public CmdExecutor
+{
+    enum ParamAddress : int
+    {
+        first = 1,
+        second = 2,
+        third = 3
+    };
+public:
+    long paramsLength() override { return 4; }
+    void execute(vector<long>::iterator begginingOfInstruction,
+                         vector<long>& computerMemory) override
+    {
+        const long leftParam = *(begginingOfInstruction + ParamAddress::first);
+        const long rightParam = *(begginingOfInstruction + ParamAddress::second);
+        const long destParam = *(begginingOfInstruction + ParamAddress::third);
+        computerMemory[destParam] = computerMemory[leftParam] * computerMemory[rightParam];
+    }
+};
+
+class Halt : public CmdExecutor
+{
+public:
+    long paramsLength() override { return 1; }
+    void execute(vector<long>::iterator, vector<long>&) override
+    {
+        IntCodeComputer::halt();
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// main
 int main()
 {
     int N;
     cin >> N;
-    int M;
-    cin >> M;
 
-    // size requirement
-    const int max_pass = 999999;
-    if (N > max_pass)
-        N = max_pass;
-    if (M > max_pass)
-        M = max_pass;
-
-    vector<string> sutiablePsswds;
-    for(int i = N; i != M; ++i)
+    IntCodeComputer computer{};
+    for(int input = 0; input != N; ++input)
     {
-        std::string input{to_string(i)};
-        if (passwordValidation(input))
-            sutiablePsswds.push_back(std::move(input));
+        long val;
+        cin >> val;
+        computer.memory.push_back(val);
+        char separator;
+        cin >> separator;
     }
+    computer.setMemory();
 
-    cout << "All paswords are: " << endl;
-    for_each(sutiablePsswds.cbegin(), sutiablePsswds.cend(),
-             [](const auto& el){ cout << el << ", "; });
-    cout << endl << "Answer is " << sutiablePsswds.size() << endl;
+    computer.printMemory();
+    // register instructions
+    computer.instructionSet.insert({OpcodeInstruction::multiply, CmdExecutorPtr{new Multiply()}});
+    computer.instructionSet.insert({OpcodeInstruction::sum, CmdExecutorPtr{new Sum()}});
+    computer.instructionSet.insert({OpcodeInstruction::halt, CmdExecutorPtr{new Halt()}});
+
+    const long result = computer.runningLoop(0);
+    computer.printMemory();
+    cout << "Return code is " << result << endl;
     return 0;
 }
 
