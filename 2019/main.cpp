dc6e325a0d344a47ad57f573cef8b84d17b96e99
nikolayp <nikolay.a.petrov@gmail.com>
2019-12-08 20:00:46 +0100

    Day 8 part 2

--- 2019/main.cpp
+++ 2019/main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <vector>
-#include <map>
-#include <memory>
+#include <array>
 using namespace std;
 
 constexpr long wide = 25;
@@ -31,40 +29,22 @@ int main()
 
     long numLayers = input.size() / (wide * tall);
     //cerr << "numLayers " << numLayers << endl;
-    vector<LayerInfo> data;
-    //data.resize(numLayers);
-    for(long layer = 0; layer != numLayers; ++layer)
+    for(long px = 0; px != wide*tall; ++px)
     {
-        long zeros = 0, ones = 0, twos = 0;
-        for(long px = 0; px != wide*tall; ++px)
+        for(long layer = 0; layer != numLayers; ++layer)
         {
             const long pos = layer*wide*tall + px;
-            switch (input[pos])
+            if (input[pos] != '2') // transparent
             {
-                case '0':
-                    ++zeros;
-                    break;
-                case '1':
-                    ++ones;
-                    break;
-                case '2':
-                    ++twos;
-                    break;
-                //default:
-                //    cerr << "Not interested in this color!" << endl;
+                // 0 is black, 1 is white
+                const char res = input[pos] == '0' ? '.' : '*';
+                cout << res;
+                if ((px + 1) % wide == 0)
+                    cout << endl;
+                break;
             }
         }
-        data.push_back(LayerInfo{layer, zeros, ones, twos});
     }
-    for_each(data.cbegin(), data.cend(), [](const auto& el){ cerr << el << endl;});
-
-    sort(data.begin(), data.end(),
-            [](const auto& el, const auto& anotherEl)
-            { return el.zeros < anotherEl.zeros; });
-    auto minLayer = data.front();
-    cout << "The layer with least zeros is " << minLayer.index << endl;
-    cout << "The number of 1 digits multiplied by the number of 2 digits at this layer is ";
-    cout << minLayer.ones * minLayer.twos << endl;
     return 0;
 }
 
