71d01b7253ab762a97f9c36137a4fa7b7982f2b3
nikolayp <nikolay.a.petrov@gmail.com>
2019-12-13 23:40:58 +0100

    Add tests for day7 part 2 to intcode computer

--- 2019/intcode/tst/intcode_test.cpp
+++ 2019/intcode/tst/intcode_test.cpp
@@ -167,3 +167,60 @@ TEST(IntCodeComputerTest, AmplificationDay7) {
         {1,0,4,3,2});
 }
 
+void simpleAmplifierLoopTestHelper(std::string message,
+                                   std::vector<long> input,
+                                   long expectedOutput,
+                                   std::vector<long> expectedPhases)
+{
+    vector<long> phaseSettings{5,6,7,8,9};
+    vector<IntCodeComputer> amplifiers{};
+    pair<vector<long>, long> result{phaseSettings, -222};
+    do
+    {
+        amplifiers.clear();
+        for(int j = 0; j != phaseSettings.size(); ++j)
+        {
+            amplifiers.emplace_back("", input);
+            amplifiers.back().pausable = true;
+            setUpInstructions(amplifiers.back().instructionSet);
+            amplifiers.back().setPhase(phaseSettings[j]);
+        }
+        long output = 0;
+        IntCodeComputer::State state = IntCodeComputer::Running;
+        int i = 0;
+        while(i != amplifiers.size())
+        {
+            std::tie(output, state) = amplifiers[i].runningLoop(output);
+            if (state == IntCodeComputer::Halt)
+                break;
+            if (i == amplifiers.size() - 1)
+            {
+                i = 0;
+                continue;
+            }
+            ++i;
+        }
+        if (output > result.second)
+            result = {phaseSettings, output};
+    } while(std::next_permutation(phaseSettings.begin(), phaseSettings.end()));
+    ASSERT_EQ(result.first, expectedPhases);
+    ASSERT_EQ(result.second, expectedOutput);
+}
+
+TEST(IntCodeComputerTest, AmplificationLoopDay7) {
+    simpleAmplifierLoopTestHelper(
+        "Max thruster signal 139629729 (from phase setting sequence 9,8,7,6,5)",
+        {3,26,1001,26,-4, 26,3,27,1002,27, 2,27,1,27,26, 27,4,27,1001,28, -1,28,1005,28,6, 99,0,0,5},
+        139629729,
+        {9,8,7,6,5});
+
+    simpleAmplifierLoopTestHelper(
+        "Max thruster signal 18216 (from phase setting sequence 9,7,8,5,6)",
+        {3,52,1001,52,-5, 52,3,53,1,52, 56,54,1007,54,5, 55,1005,55,26,1001,
+         54,-5,54,1105,1, 12,1,53,54,53, 1008,54,0,55,1001, 55,1,55,2,53,
+         55,53,4,53,1001, 56,-1,56,1005,56, 6,99,0,0,0, 0,10},
+        18216,
+        {9,7,8,5,6});
+}
+
+
