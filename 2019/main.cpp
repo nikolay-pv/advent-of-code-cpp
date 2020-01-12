c2e54f82da5bcb9386f83d8caa92216e1ab2ebb2
nikolayp <nikolay.a.petrov@gmail.com>
2020-01-12 17:19:00 +0100

    Task 22 part 2

--- 2019/main.cpp
+++ 2019/main.cpp
@@ -17,68 +17,168 @@
 #include <queue>
 
 using namespace std;
-using coord = std::pair<long, long>;
+using bigint = __int128;
+using coord = std::pair<bigint, bigint>;
+
+std::ostream& operator<<(std::ostream& os, const bigint& obj)
+{
+    os << static_cast<long long>(obj);
+    return os;
+}
+
+bigint mod(bigint a, bigint m)
+{ return a < 0 ? (a + m) % m : a % m; }
+
+// Modular exponentiation
+bigint modpow(bigint x, bigint e, bigint m)
+{
+    if (e == 0) {
+        return (x == 0) ? 0 : 1;
+    }
+    x = mod(x, m);
+    bigint res{1};
+    while (e > 0)
+    {
+        // If e is odd, multimle x with result
+        if (e % 2 != 0)
+            res = mod(res * x, m);
+        // e is even now
+        e /= 2;
+        x = mod(x * x, m);
+    }
+    return res;
+}
+
+// extended Euclidean Algorithm
+tuple<bigint, bigint, bigint> gcdExtended(bigint a, bigint m)
+{
+    // search ax+my=gcd(a,m)
+    // base of recursion
+    if (a == 0)
+        return {m, 0, 1};
+
+    auto[gcd, x, y] = gcdExtended(m % a, a);
+    return {gcd, y - (m / a) * x, x};
+}
+
+bigint modInverse(bigint a, bigint m)
+{
+    // ax + my = g <=> a*y = 1 mod (m)
+    auto[g, x, y] = gcdExtended(a, m);
+    bigint x1, y1;
+    assert(g == 1);
+    return mod(x, m);
+}
+
+bigint modDivide(bigint a, bigint b, bigint m)
+{
+    a = mod(a, m);
+    bigint inv = modInverse(b, m);
+    return (a * inv) % m;
+}
 
 struct DeckShuffler
 {
-    long deckSize{10};
+    bigint deckSize{10};
+    // store the equation: newIdx = a*oldIdx + b
+    bigint a{1};
+    bigint b{0};
 
-    long deal(long index)
+    // every new operation: newIdx = a2 * (a*oldIdx + b) + b2 <=> a2*a*oldIdx + a2*b + b2
+    // then % deckSize is taken
+    void applyNewFunc(bigint a2, bigint b2)
     {
-        //cerr << "Dealing index " << index << endl;
-        return deckSize - index - 1;
+        a = mod(a * a2, deckSize);
+        b = mod(a2 * b + b2, deckSize);
     }
 
-    long reverseDeal(long index)
+    bigint evalFunc(bigint index)
+    { return mod(mod(a*index, deckSize) + b, deckSize); }
+
+    bigint evalTimesAndReverse(bigint times, bigint finalIdx)
     {
-        return deckSize - index - 1;
+        // a*a*oldIdx + a*b + b per iteration
+        const bigint pwrA = modpow(a, times, deckSize);
+        const bigint newA{pwrA};
+        assert(newA < deckSize);
+        // Fermat's little theorem to calculate modInverse of a - 1
+        const bigint inv{modpow(a - 1, deckSize - 2, deckSize)};
+        assert(inv < deckSize);
+        // geometric progression b * (a^times - 1) / (a - 1)
+        bigint newB{pwrA - 1};
+        newB = modDivide(newB, a - 1, deckSize);
+        newB *= b;
+        newB = mod(newB, deckSize);
+        bigint originalIdx{mod(finalIdx - newB, deckSize)};
+        originalIdx = modDivide(originalIdx, newA, deckSize);
+        return mod(originalIdx, deckSize);
     }
 
-    long cut(long n, long index)
+    bigint deal(bigint index)
     {
-        //cerr << "Cutting index " << index << endl;
+        const bigint newIdx{deckSize - index - 1};
+        applyNewFunc(-1, deckSize - 1);
+        return newIdx;
+    }
+
+    bigint cut(bigint n, bigint index)
+    {
+        applyNewFunc(1, -n);
         index -= n;
         if (index >= deckSize)
-            index %= deckSize;
+            index = mod(index, deckSize);
         while(index < 0)
             index += deckSize;
         return index;
     }
 
-    long reverseCut(long n, long index)
+    bigint deal(bigint n, bigint index)
     {
-        index += n;
-        if (n > 0)
-            index %= deckSize;
-        else if (index < 0)
-            index = deckSize + index;
-        return index;
+        assert(n != 0);
+        applyNewFunc(n, 0);
+        return mod(n * index, deckSize);
     }
 
-    long deal(long n, long index)
+    bigint traverseInstructions(vector<string> const& inputs, bigint target)
     {
-        //cerr << "Dealing index " << index << " with n " << n << endl;
-        return (n * index) % deckSize;
+        for(auto const& input : inputs)
+            target = parseInput(input, target);
+        return target;
     }
 
-    long reverseDeal(long n, long index)
+    bigint parseInput(string const& input, bigint target)
     {
-        if (index % n == 0)
-            return index / n;
-        const long circle = n - index % n;
-        const long offsettedPosition = circle*deckSize + index;
-        const long answ = offsettedPosition / n;
-        //cerr << "circle " << circle << " offsettedPosition " << offsettedPosition << " answ " << answ << endl;
-        return answ;
+        auto keyword = input.substr(0, 4);
+        if (keyword == "deal")
+        {
+            if (auto second = input.substr(5, 4); second == "with") {
+                auto pos = input.rfind(' ');
+                auto num = input.substr(pos, input.size() - pos);
+                const bigint n = stol(num);
+                target = deal(n, target);
+            } else {
+                target = deal(target);
+            }
+        }
+        else if (keyword == "cut ")
+        {
+            auto num = input.substr(input.rfind(' '));
+            const bigint n = stol(num);
+            target = cut(n, target);
+        }
+        else
+            cerr << "shuffle overflow" << endl;
+        return target;
     }
+
 
     // for tests
-    static vector<long> deal(vector<long>& deck)
+    static vector<bigint> deal(vector<bigint>& deck)
     {
         return {deck.rbegin(), deck.rend()};
     }
 
-    static vector<long> cut(long n, vector<long> deck)
+    static vector<bigint> cut(bigint n, vector<bigint> deck)
     {
         if (n > 0)
             rotate(deck.begin(), deck.begin() + n, deck.end());
@@ -87,95 +187,15 @@ struct DeckShuffler
         return std::move(deck);
     }
 
-    static vector<long> deal(long n, vector<long>& deck)
+    static vector<bigint> deal(bigint n, vector<bigint>& deck)
     {
-        long newPos = 0;
-        vector<long> ndeck(deck.size());
-        for (int i = 0; i != deck.size(); ++i, newPos += n)
-            ndeck[newPos % deck.size()] = deck[i];
+        bigint newPos = 0;
+        vector<bigint> ndeck(deck.size());
+        for (bigint i = 0; i != deck.size(); ++i, newPos += n)
+            ndeck[mod(newPos, deck.size())] = deck[i];
         return std::move(ndeck);
     }
 
-    long traverseInstructions(vector<string> const& inputs, long target)
-    {
-        for(auto const& input : inputs)
-            target = parseInput(input, target);
-        return target;
-    }
-
-    long parseInput(string const& input, long target)
-    {
-        //cerr << "Input " << input << endl;
-        auto keyword = input.substr(0, 4);
-        if (keyword == "deal")
-        {
-            if (auto second = input.substr(5, 4); second == "with") {
-                auto pos = input.rfind(' ');
-                auto num = input.substr(pos, input.size() - pos);
-                const long n = stol(num);
-                //cerr << "Extracted " << n << " from " << input << endl;
-                target = deal(n, target);
-                //cerr << " result = " << target << endl;
-            } else {
-                //cerr << "Extracted from " << input << endl;
-                target = deal(target);
-                //cerr << " result = " << target << endl;
-            }
-        }
-        else if (keyword == "cut ")
-        {
-            auto num = input.substr(input.rfind(' '));
-            const long n = stol(num);
-            //cerr << "Extracted " << n << " from " << input << endl;
-            target = cut(n, target);
-            //cerr << " result = " << target << endl;
-        }
-        else
-            cerr << "shuffle overflow" << endl;
-        return target;
-    }
-
-    long reverseTraverseInstructions(vector<string> const& inputs, long target)
-    {
-        for(auto const& input : inputs)
-            target = reverseInput(input, target);
-        return target;
-    }
-
-    long reverseInput(string const& input, long target)
-    {
-        //deal into new stack
-        //cut -2
-        //deal with increment 7
-        //cut 8
-        //cut -4
-        //deal with increment 7
-        //cut 3
-        //deal with increment 9
-        //deal with increment 3
-        auto keyword = input.substr(0, 4);
-        if (keyword == "deal")
-        {
-            if (auto second = input.substr(4, 4); second == "with") {
-                auto num = input.substr(input.rfind(' '));
-                const long n = stol(num);
-                //cerr << "Extracted " << n << " from " << input << endl;
-                target = reverseDeal(n, target);
-            } else {
-                target = reverseDeal(target);
-            }
-        }
-        else if (keyword == "cut ")
-        {
-            auto num = input.substr(input.rfind(' '));
-            const long n = stol(num);
-            //cerr << "Extracted " << n << " from " << input << endl;
-            target = reverseCut(n, target);
-        }
-        else
-            cerr << "shuffle overflow" << endl;
-        return target;
-    }
 };
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -196,65 +216,61 @@ int main()
     }
 
     {
-    DeckShuffler testShuffler{10};
-    vector<long> testDeck{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    // deal
-    vector<long> res{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-    assert(res == DeckShuffler::deal(testDeck));
-    long resIdx = 0;
-    assert(res[resIdx] == testDeck[testShuffler.reverseDeal(resIdx)]);
-    assert(resIdx == testShuffler.deal(res[resIdx]));
-    resIdx = 3;
-    assert(res[resIdx] == testDeck[testShuffler.reverseDeal(resIdx)]);
-    assert(resIdx == testShuffler.deal(res[resIdx]));
-    for(resIdx = 0; resIdx != res.size(); ++resIdx)
-    {
-        //cerr << "checking " << resIdx << endl;
-        assert(res[resIdx] == testDeck[testShuffler.reverseDeal(resIdx)]);
+        DeckShuffler testShuffler{10};
+        vector<bigint> testDeck{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        // deal
+        vector<bigint> res{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        assert(res == DeckShuffler::deal(testDeck));
+        bigint resIdx = 0;
         assert(resIdx == testShuffler.deal(res[resIdx]));
-    }
-    // cut
-    res = {3, 4, 5, 6, 7, 8, 9, 0, 1, 2};
-    assert(res == DeckShuffler::cut(3, testDeck));
-    resIdx = 0;
-    assert(res[resIdx] == testDeck[testShuffler.reverseCut(3, resIdx)]);
-    assert(resIdx == testShuffler.cut(3, res[resIdx]));
-    resIdx = 4;
-    assert(res[resIdx] == testDeck[testShuffler.reverseCut(3, resIdx)]);
-    assert(resIdx == testShuffler.cut(3, res[resIdx]));
-    for(resIdx = 0; resIdx != res.size(); ++resIdx)
-    {
-        //cerr << "checking " << resIdx << endl;
-        assert(res[resIdx] == testDeck[testShuffler.reverseCut(3, resIdx)]);
+        resIdx = 3;
+        assert(resIdx == testShuffler.deal(res[resIdx]));
+        for(resIdx = 0; resIdx != res.size(); ++resIdx)
+            assert(resIdx == testShuffler.deal(res[resIdx]));
+        // cut
+        res = {3, 4, 5, 6, 7, 8, 9, 0, 1, 2};
+        assert(res == DeckShuffler::cut(3, testDeck));
+        resIdx = 0;
         assert(resIdx == testShuffler.cut(3, res[resIdx]));
-    }
-    res = {6, 7, 8, 9, 0, 1, 2, 3, 4, 5};
-    assert(res == DeckShuffler::cut(-4, testDeck));
-    for(resIdx = 0; resIdx != res.size(); ++resIdx)
-    {
-        cerr << "checking " << resIdx << endl;
-        assert(res[resIdx] == testDeck[testShuffler.reverseCut(-4, resIdx)]);
-        assert(resIdx == testShuffler.cut(-4, res[resIdx]));
-    }
-    // deal with increment
-    res = {0, 7, 4, 1, 8, 5, 2, 9, 6, 3};
-    assert(res == DeckShuffler::deal(3, testDeck));
-    for(resIdx = 0; resIdx != res.size(); ++resIdx)
-    {
-        //cerr << "checking " << resIdx << endl;
-        assert(res[resIdx] == testDeck[testShuffler.reverseDeal(3, resIdx)]);
-        assert(resIdx == testShuffler.deal(3, res[resIdx]));
-    }
+        resIdx = 4;
+        assert(resIdx == testShuffler.cut(3, res[resIdx]));
+        for(resIdx = 0; resIdx != res.size(); ++resIdx)
+            assert(resIdx == testShuffler.cut(3, res[resIdx]));
+        res = {6, 7, 8, 9, 0, 1, 2, 3, 4, 5};
+        assert(res == DeckShuffler::cut(-4, testDeck));
+        for(resIdx = 0; resIdx != res.size(); ++resIdx)
+            assert(resIdx == testShuffler.cut(-4, res[resIdx]));
+        // deal with increment
+        res = {0, 7, 4, 1, 8, 5, 2, 9, 6, 3};
+        assert(res == DeckShuffler::deal(3, testDeck));
+        for(resIdx = 0; resIdx != res.size(); ++resIdx)
+            assert(resIdx == testShuffler.deal(3, res[resIdx]));
     }
 
-    // part 1
     cout << "Part 1\n";
-    DeckShuffler part1{10007};
-    const long initialIdx = 2019;
-    const long answ = part1.traverseInstructions(inputs, initialIdx);
-    cout << "the position of card " << initialIdx << " is " << answ << endl;
-    // part 2
+    {
+        DeckShuffler part1{10007};
+        const bigint initialIdx = 2019;
+        const bigint answ = part1.traverseInstructions(inputs, initialIdx);
+        cout << "The position of card " << initialIdx << " is " << answ << endl;
+    }
+
     cout << "Part 2\n";
+    {
+        DeckShuffler part2{119315717514047};
+        part2.traverseInstructions(inputs, 0);
+
+        const bigint times{101741582076661};
+        const bigint finalIdx = 2020;
+        const bigint answ = part2.evalTimesAndReverse(times, finalIdx);
+        cout << "The original position of card " << finalIdx << " is " << answ << endl;
+        assert(answ < 72238684032294);
+        assert(answ > 35289);
+        assert(answ != 72238684017424);
+        assert(answ != 72238684032293);
+        assert(answ != 95709592746680);
+        assert(answ != 45542206702723);
+    }
 
     return 0;
 }
